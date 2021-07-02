c8ae8acec7d53498e093eabfe8a64acea42416d8
Ritesh Kumar <riteshraj1014@gmail.com>
2021-07-02 23:44:16 +0530

    Initial commit

---
+++ frontend/src/screens/AboutScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './AboutScreen.css'
+const AboutScreen = () => {
+    return (
+        <div>
+            <h1>WELCOME TO <br></br>NATIONAL INSTITUE OF TECHNOLOGY<br></br>CALICUT </h1>,
+            <br></br>
+            <h2>WE ARE GROUP OF MERN STACK DEVELOPER </h2>
+        </div>
+    )
+}
+
+export default AboutScreen
